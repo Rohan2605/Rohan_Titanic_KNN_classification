ce29902af2a035ec9e0fdb61d82c7876bc5e938c
Rohan2605 <83447838+Rohan2605@users.noreply.github.com>
2022-08-01 23:46:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def predict():
       return render_template('index.html', prediction_text='Thank GOD, the person you are searching for is safe ')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
